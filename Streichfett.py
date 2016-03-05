c2cbfe4cae5771346d69104785ff6858cdc397ea
arru <arru@users.noreply.github.com>
2016-03-05 19:51:47 +0100

    Avoid _get_name choking on Streichfett panel patch data

--- Streichfett.py
+++ Streichfett.py
@@ -9,9 +9,9 @@ class StreichfettPatch(SysexPatch.SysexPatch):
 	BANK_PANEL = 0x7f  # Play / edit buffer
 	NULL_DEVICE = 0x7f
 
-	BANK_LETTERS = ['A', 'B', 'C']
 	DEFAULT_PORT_NAME = "Streichfett"
 
+	BANK_LETTERS = ['A', 'B', 'C']
 
 
 	def _send_request(self, port):
@@ -20,11 +20,15 @@ class StreichfettPatch(SysexPatch.SysexPatch):
 		out_port.send(request)
 
 	def _get_name(self):
-		patch_code = self._data[4]
-		bank_num = patch_code // 4
-		patch_num = (patch_code % 4) + 1
-		bank = self.BANK_LETTERS[bank_num]
-		return "%s%d" % (bank, patch_num)
+		patch_code = list(self._data[0].data)[4]
+
+		if patch_code == 127:
+			return "(panel)"
+		else:
+			bank_num = patch_code // 4
+			patch_num = (patch_code % 4) + 1
+			bank = self.BANK_LETTERS[bank_num]
+			return "%s%d" % (bank, patch_num)
 
 	@classmethod
 	def _verify(cls, msg_list):
