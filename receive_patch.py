841117f3a8091fc409965f60a55a2b727c6e7f0d
arru <arru@users.noreply.github.com>
2016-03-05 21:43:21 +0100

    Receive_patch.py command

---
+++ receive_patch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import importlib
+import mido
+import sys
+import re
+
+module_name = re.match("^[a-zA-Z_]+$", sys.argv[1]).group(0)
+class_name = "%sPatch" % (module_name)
+if len (sys.argv) == 2:
+	portname = None
+else:
+	portname = sys.argv[2]
+
+module = importlib.import_module(module_name)
+
+device_class = getattr(module, class_name)
+try:
+	patch = device_class(portname)
+except IOError:
+	"\n".join(mido.get_input_names())
+	raise
+
+print "Received patch %s, done" % patch.get_name()
+
+patch.write_syx("%s.syx" % patch.get_name())
